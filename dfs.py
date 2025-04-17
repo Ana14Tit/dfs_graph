5d0f89ada58ec5af7920036ff6574c5c5e91dcd3
Anastasiya Titova <atitova1401@gmail.com>
2025-04-17 14:11:18 +0300

    feat: Add exception handling start/end nodes

--- dfs.py
+++ dfs.py
@@ -1,15 +1,27 @@
-def dfs(graph, start_node, visited=None):
+def dfs_path_length(graph, start_node, end_node, visited=None, path=None):
+    if start_node not in graph:
+        raise ValueError(f"Начальная вершина {start_node} не найдена в графе.")
+    if end_node not in graph:
+        raise ValueError(f"Конечная вершина {end_node} не найдена в графе.")
+
+
     if visited is None:
         visited = set()
-    visited.add(start_node)
-    path = [start_node]
+    if path is None:
+        path = [start_node]
 
+    visited.add(start_node)
+
+    if start_node == end_node:
+        return len(path) -1  
     for neighbor in graph[start_node]:
         if neighbor not in visited:
-            path.extend(dfs(graph, neighbor, visited))
-            break 
-    return path
+            new_path = path + [neighbor]
+            result = dfs_path_length(graph, neighbor, end_node, visited, new_path)
+            if result is not None:
+                return result
 
+    return None  
 
 if __name__ == '__main__':
     edges = [(4, 2), (1, 3), (2, 4)]
@@ -21,7 +33,11 @@ if __name__ == '__main__':
             graph[v] = []
         graph[u].append(v)
         graph[v].append(u)  
-    print(f"Graph representation: {graph}")
-    start_node = 1
-    path = dfs(graph, start_node)
-    print(f"DFS обход из вершины {start_node}: {path}")
+
+    start_node = 2
+    end_node = 5  
+    try:
+        path_length = dfs_path_length(graph, start_node, end_node)
+        print(f"Длина пути от вершины {start_node} до вершины {end_node}: {path_length}")
+    except ValueError as e:
+        print(f"Ошибка: {e}")
