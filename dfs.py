848e43629a5e7814ebcc6fd7c0b83e4233b9543a
Anastasiya Titova <atitova1401@gmail.com>
2025-04-17 13:33:26 +0300

    Initial commit: Implement DFS algorithm

---
+++ dfs.py
@@ -0,0 +1,24 @@
+def dfs(graph, start_node, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start_node)
+    path = [start_node]
+    for neighbor in graph[start_node]:
+        if neighbor not in visited:
+            path.extend(dfs(graph, neighbor, visited))
+            break 
+    return path
+if __name__ == '__main__':
+    edges = [(4, 2), (1, 3), (2, 4)]
+    graph = {}
+    for u, v in edges:
+        if u not in graph:
+            graph[u] = []
+        if v not in graph:
+            graph[v] = []
+        graph[u].append(v)
+        graph[v].append(u)  
+    print(f"Graph representation: {graph}")
+    start_node = 1
+    path = dfs(graph, start_node)
+    print(f"DFS обход из вершины {start_node}: {path}")
